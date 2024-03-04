c754df95d3f970c32ba4931dbbbb87cb6fa53e84
Albert Abdon <albert,abdon@student.ateneo.edu>
2024-03-04 21:15:54 +0800

    added the 3 models: ingredient, recipe, recipeingredient and revised format

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.urls import reverse
+
+class Ingredient(models.Model):
+  name = models.CharField(max_length=50)
+  
+  
+  
+class Recipe(models.Model):
+  name = models.CharField(max_length=50)
+  
+  
+  
+class RecipeIngredient(models.Model):
+  quantity = models.CharField(max_length=50)
+  ingredient = models.ForeignKey(
+      Ingredient,
+      on_delete=models.CASCADE,
+      related_name = "recipe",
+   )  
+  recipe = models.ForeignKey(
+      Recipe,
+      on_delete=models.CASCADE,
+      related_name = "ingredient",
+   )  
 
 # Create your models here.
